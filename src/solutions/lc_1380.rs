7dd1e656446dd9f965295b275a2f957c2e54e44d
Simon Braitsch <S.Braitsch@gmx.de>
2024-07-19 15:58:04 +0200

    refactor. performance now 100% rt/mem

--- src/solutions/lc_1380.rs
+++ src/solutions/lc_1380.rs
@@ -1,14 +1,25 @@
 use crate::structs::solution::Solution;
+use std::cmp::max;
 
 impl Solution {
     pub fn lucky_numbers(matrix: Vec<Vec<i32>>) -> Vec<i32> {
-        let mut lucky = vec![];
+        let mut lucky = Vec::with_capacity(1);
+        let mut cmax = vec![0; matrix[0].len()];
         matrix
             .iter()
-            .map(|row| row.iter().enumerate().map(|(i, v)| (*v, i)).min().unwrap())
+            .map(|row| {
+                row.iter()
+                    .enumerate()
+                    .map(|(i, v)| {
+                        cmax[i] = max(cmax[i], *v);
+                        (*v, i)
+                    })
+                    .min()
+                    .unwrap()
+            })
             .max()
             .inspect(|(val, c_id)| {
-                if !matrix.iter().any(|row| row[*c_id] > *val) {
+                if cmax[*c_id] == *val {
                     lucky.push(*val);
                 }
             });
