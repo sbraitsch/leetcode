3e0579a3d66496ac163e4d98b75724cae007cde4
s-braitsch <simon.braitsch@novatec-gbmh.de>
2024-08-02 11:10:58 +0200

    adjusted

--- src/solutions/lc_2134.rs
+++ src/solutions/lc_2134.rs
@@ -4,7 +4,10 @@ use std::cmp::min;
 impl Solution {
     pub fn min_swaps(nums: Vec<i32>) -> i32 {
         let window_size = nums.iter().sum::<i32>() as usize;
-        let mut swap: i32 = window_size as i32 - nums[0..window_size].iter().sum::<i32>();
+        if window_size == 0 || window_size == nums.len() {
+            return 0;
+        }
+        let mut swap: i32 = window_size as i32 - nums.iter().take(window_size).sum::<i32>();
         let mut res = swap;
         for i in 1..nums.len() {
             swap += nums[i - 1] - nums[(i + (window_size - 1)) % nums.len()];
